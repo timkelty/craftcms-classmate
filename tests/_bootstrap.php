b4b2327e20fab277883476afa452f22585e4a1fb
Tim Kelty <tim@fusionary.com>
2021-01-18 21:34:02 -0500

    test env

--- tests/_bootstrap.php
+++ tests/_bootstrap.php
@@ -4,14 +4,12 @@ use craft\test\TestSetup;
 
 ini_set('date.timezone', 'UTC');
 
-// Use the current installation of Craft
+define('CRAFT_TESTS_PATH', __DIR__);
 define('CRAFT_STORAGE_PATH', __DIR__ . '/_craft/storage');
 define('CRAFT_TEMPLATES_PATH', __DIR__ . '/_craft/templates');
 define('CRAFT_CONFIG_PATH', __DIR__ . '/_craft/config');
 define('CRAFT_MIGRATIONS_PATH', __DIR__ . '/_craft/migrations');
 define('CRAFT_TRANSLATIONS_PATH', __DIR__ . '/_craft/translations');
-define('CRAFT_VENDOR_PATH', dirname(__DIR__) . '/vendor');
-
-$devMode = true;
+define('CRAFT_VENDOR_PATH', dirname(__DIR__).'/vendor');
 
 TestSetup::configureCraft();
--- tests/unit/ExampleUnitTest.php
+++ tests/unit/ExampleUnitTest.php
@@ -1,65 +1,21 @@
 <?php
-/**
- * Classmate plugin for Craft CMS 3.x
- *
- * HTML class manager
- *
- * @link      http://github.com/timkelty
- * @copyright Copyright (c) 2021 Tim Kelty
- */
 
 namespace timkelty\classmatetests\unit;
 
 use Codeception\Test\Unit;
-use UnitTester;
 use Craft;
-use timkelty\classmate\Classmate;
+use timkelty\craftcms\classmate\Plugin;
+use UnitTester;
 
-/**
- * ExampleUnitTest
- *
- *
- * @author    Tim Kelty
- * @package   Classmate
- * @since     0.1.0
- */
 class ExampleUnitTest extends Unit
 {
-    // Properties
-    // =========================================================================
+    protected UnitTester $tester;
 
-    /**
-     * @var UnitTester
-     */
-    protected $tester;
-
-    // Public methods
-    // =========================================================================
-
-    // Tests
-    // =========================================================================
-
-    /**
-     *
-     */
     public function testPluginInstance()
     {
         $this->assertInstanceOf(
-            Classmate::class,
-            Classmate::$plugin
-        );
-    }
-
-    /**
-     *
-     */
-    public function testCraftEdition()
-    {
-        Craft::$app->setEdition(Craft::Pro);
-
-        $this->assertSame(
-            Craft::Pro,
-            Craft::$app->getEdition()
+            Plugin::class,
+            Plugin::getInstance()
         );
     }
 }
